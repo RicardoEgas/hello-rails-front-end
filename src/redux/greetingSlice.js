882cf0a3d3b6a6f4a8da88f09f594991f2fd372d
RicardoEgas <rj_egas@hotmail.com>
2023-12-23 05:20:16 +0100

    Add react files and fetch API

---
+++ src/redux/greetingSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchRandomGreeting = createAsyncThunk(
+  'greeting/fetchRandomGreeting',
+  async () => {
+    const response = await axios.get('http://localhost:3000/api/greetings/random');
+
+    return response.data;
+  }
+);
+
+const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState: {
+    data: null,
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRandomGreeting.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchRandomGreeting.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.data = action.payload;
+      })
+      .addCase(fetchRandomGreeting.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
